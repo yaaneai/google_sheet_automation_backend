875131e180a47e78208cc452443d37b7eb00ad9e
Vicky226521 <vicky226521@gmail.com>
2025-03-21 10:36:31 +0530

    RLB Automation MVP done

---
+++ AutomationScript.py
@@ -0,0 +1,106 @@
+from openpyxl import load_workbook as LWB
+from combainedExcel import getFormattedSheet
+import pandas as pd
+import os
+import json
+import re
+directory_path = "C:\\Users\\Vignesh\\Documents\\RLB Automations\\files"
+xlsx_files = [f for f in os.listdir(directory_path) if f.endswith(".xlsx")]
+header = ['ITEM','DESCRIPTION','QUANTITY','UNIT','RATE']
+if len(xlsx_files) > 0:
+    workbook = []
+    for file in xlsx_files:
+        workbook.append(pd.ExcelFile(f'{directory_path}\\{file}', engine='openpyxl'))
+    sheet_name = ["B2-Sec C"]#workbook[0].sheet_names[2:len(workbook[0].sheet_names)-1]#get the list of sheet names from the uploaded contrancter file"
+    for sheet in sheet_name:
+        combainedJson={
+        "sheet":sheet,
+        "title":"",
+        "page":[]
+        }
+        pdsheet=pd.read_excel(workbook[0],sheet)
+        cleaned_data = pdsheet.dropna(how='all').dropna(axis=1, how='all')
+        page_name_array = []
+        page_no = 0
+        for index, row in cleaned_data.iterrows():
+            page_n = row.dropna().values[0]
+            if isinstance(page_n,str):
+                if "PAGE" in page_n:
+                    page_name_array.append(page_n)
+                    if page_n[-1] == "S":
+                        break
+        title1 = cleaned_data.loc[:, ~cleaned_data.columns.str.contains('^Unnamed')].columns[0]
+        title2 = cleaned_data.iloc[0].dropna().values[0]
+        title3 = cleaned_data.iloc[1].dropna().values[0]
+        title4 = cleaned_data.iloc[2].dropna().values[0]
+        title5 = cleaned_data.iloc[3].dropna().values[0]
+        combainedJson["title"] = f"{title1}\n{title2}\n{title3}\n{title4}\n{title5}"
+        for page in page_name_array:
+            page_info_item = {
+                "page_name":"",
+                "Contractor":[],
+                }
+            #goto_page = page_name_array.index(page)+1
+            page_info_item["page_name"] = page
+            for contractor in workbook:
+                #current_page = 1
+                # wb = pd.ExcelFile(f'{directory_path}\\{contractor}', engine='openpyxl')
+                sheetdata=pd.read_excel(contractor,sheet)
+                preprocessed_data = sheetdata.dropna(how='all').dropna(axis=1, how='all')
+                preprocessed_data.reset_index(drop=True, inplace=True)
+                for index, row in preprocessed_data.iterrows():
+                    if (set(row.values) & set(header)) and preprocessed_data.iloc[index+1].dropna().values[0] == page:
+                        Contractor_page_info_item = {
+                            "contractor_name":"",
+                            "bill_section_header":[],
+                            "bill_section_items":[],
+                            "total":""
+                            }
+                        Contractor_page_info_item["contractor_name"]=os.path.splitext(os.path.split(contractor)[1])[0]
+                        if pd.isna(row.values[5]):
+                            preprocessed_data.at[index, preprocessed_data.columns[5]] = 'AMOUNT SAR RIYAL'
+                            inx=index+1
+                            non_billable_description = ""
+                            item_array = []
+                        while not(set(preprocessed_data.iloc[inx].values) & set(header)) and inx < len(preprocessed_data)-1:#get into quotation data
+                            def bill_section():
+                                if pd.isna(preprocessed_data.iloc[inx].values[0]) and "Total" not in preprocessed_data.iloc[inx].dropna().values[0]: #collects only bill_section_header
+                                    global non_billable_description
+                                    non_billable_description = f"{non_billable_description}\n{preprocessed_data.iloc[inx].dropna().values[0]}"
+                            bill_section() 
+                            if re.fullmatch(r"[A-Z]", str(preprocessed_data.iloc[inx].values[0])):
+                                Contractor_page_info_item["bill_section_header"].append(non_billable_description)
+                                non_billable_description = ""
+                                while re.fullmatch(r"[A-Z]", str(preprocessed_data.iloc[inx].values[0])):# true only ITEM cell has A-Z value
+                                    item_array.append({
+                                        "ITEM":preprocessed_data.iloc[inx].values[0],
+                                        "DESCRIPTION": preprocessed_data.iloc[inx].values[1],
+                                        "QUANTITY": preprocessed_data.iloc[inx].values[2],
+                                        "UNIT": preprocessed_data.iloc[inx].values[3],
+                                        "RATE": preprocessed_data.iloc[inx].values[4],
+                                        "AMOUNT SAR RIYAL": preprocessed_data.iloc[inx].values[5]
+                                        })
+                                    inx=inx+1
+                                Contractor_page_info_item["bill_section_items"].append(item_array)
+                                item_array = []
+                                bill_section()  
+                            if pd.isna(preprocessed_data.iloc[inx].values).any() and "Total" in preprocessed_data.iloc[inx].dropna().values[0]:
+                                while not(set(preprocessed_data.iloc[inx].values) & set(header)) and inx < len(preprocessed_data)-1:
+                                    total = preprocessed_data.iloc[inx].dropna().values[0]
+                                    if isinstance(total,(float,int)):
+                                        Contractor_page_info_item["total"]=total
+                                        page_info_item["Contractor"].append(Contractor_page_info_item)
+                                        break
+                                    inx=inx+1
+                                break
+                            inx=inx+1
+                    else:
+                        # if (set(row.values) & set(header)) and current_page != goto_page:
+                        #     current_page = current_page+1  
+                        continue
+                    break
+            combainedJson["page"].append(page_info_item)
+            getFormattedSheet(json.dumps(combainedJson))
+        break
+else:
+  print("no files found")
---
+++ combainedExcel.py
@@ -0,0 +1,164 @@
+# from sampleJson import formattedSheet
+import json
+from openpyxl import Workbook
+from openpyxl.styles import Alignment, PatternFill, Border, Font
+
+def increment_column(column):
+    # If column is Z, return AA
+    if column == 'Z':
+        return 'AA'
+    # If column has multiple characters
+    if len(column) == 1:
+        next_char = chr(ord(column) + 1)
+        return next_char
+    else:
+        last_char = column[-1]
+        if last_char == 'Z':
+            return increment_column(column[:-1]) + 'A'
+        else:
+            return column[:-1] + chr(ord(last_char) + 1)
+
+
+def getFormattedSheet(sheetJson):
+    formatted_josn = json.loads(sheetJson)    
+    header = list(formatted_josn["page"][0]["Contractor"][0]["bill_section_items"][0][0].keys())
+    number_of_contractors = len(formatted_josn["page"][0]["Contractor"])
+    commonColumns = header[0:len(header)-2]
+    contractorColums = header[len(header)-2:len(header)]
+    header.extend(header[len(header)-2:len(header)]*(number_of_contractors-1))
+    startColumn = 'A'
+    endColumn = ''
+    Row_num_to_insert = 1
+    for i in range(1,len(header)):
+        endColumn = increment_column(startColumn if i==1 else endColumn)
+    wb = Workbook()
+    sheet = wb.create_sheet(formatted_josn["sheet"])
+    title = str.splitlines(formatted_josn["title"])
+    # add titles in the new sheet 
+    for i in range(1,len(title)):
+        sheet.merge_cells(f"{startColumn}{i}:{endColumn}{i}")
+        sheet[f"{startColumn}{i}"]= title[i]
+        sheet[f"{startColumn}{i}"].alignment = Alignment(horizontal="center", vertical="center")
+        Row_num_to_insert = Row_num_to_insert+1
+    #creates merged cell contractor name header column
+    contracter_merge_cells_start_column = chr(ord(startColumn) + len(commonColumns))
+    contracter_merge_cells_end_column = ''
+    for i in range(1,number_of_contractors+1):
+        def contractName():
+            startCell =f"{contracter_merge_cells_start_column}{Row_num_to_insert}"
+            sheet.merge_cells(f"{startCell}:{contracter_merge_cells_end_column}{Row_num_to_insert}")
+            sheet[startCell]= formatted_josn["page"][0]["Contractor"][i-1]["contractor_name"]
+            sheet[startCell].alignment = Alignment(horizontal="center", vertical="center")
+            sheet[startCell].font = Font(bold=True)
+        if i == 1:
+            contracter_merge_cells_end_column = chr(ord(contracter_merge_cells_start_column) + 
+                                                    len(contractorColums)-1)
+            contractName()
+        else:
+            contracter_merge_cells_start_column = chr(ord(contracter_merge_cells_end_column) + 1)
+            contracter_merge_cells_end_column = chr(ord(contracter_merge_cells_start_column) + len(contractorColums)-1)
+            contractName() 
+    Row_num_to_insert=Row_num_to_insert+1
+    # getting into each page info
+    for page in formatted_josn["page"]:
+        # add header to page:
+        #page header column names
+        header_column = {
+            "ITEM":"",
+            "DESCRIPTION": "",
+            "QUANTITY": "",
+            "UNIT": "",
+            "RATE": [],
+            "AMOUNT SAR RIYAL": []
+        }
+        for i in range(1,len(header)+1):
+            column_to_insert = f"{chr(ord(startColumn) + (i-1))}" 
+            cell_to_insert = f"{column_to_insert}{Row_num_to_insert}"
+            sheet[cell_to_insert]=header[i-1]
+            sheet[cell_to_insert].font = Font(bold=True)
+            sheet[cell_to_insert].alignment = Alignment(horizontal="center", vertical="center")
+            sheet[cell_to_insert].fill = PatternFill(start_color="A6A6A6", end_color="A6A6A6", fill_type="solid")
+            if header[i-1] in commonColumns:
+                header_column[header[i-1]]= column_to_insert
+                
+            else:
+                header_column[header[i-1]].append(column_to_insert)
+        Row_num_to_insert=Row_num_to_insert+1
+        #page name under "AMOUNT SAR RIYAL" row
+        for i in range(1,number_of_contractors+1):
+            cell_to_insert = f"{chr(ord(startColumn) + (len(commonColumns)+len(contractorColums*i)-1))}{Row_num_to_insert}"
+            sheet[cell_to_insert]=page["page_name"]
+            sheet[cell_to_insert].font = Font(bold=True)
+            sheet[cell_to_insert].alignment = Alignment(horizontal="center", vertical="center")
+        Row_num_to_insert=Row_num_to_insert+1
+        #section_header and section_item
+        for section_header_counter in range(0,len(page["Contractor"][0]["bill_section_header"])):
+            section_header_split_list = str.splitlines(page["Contractor"][0]["bill_section_header"][section_header_counter])
+            for section_header_line in section_header_split_list:
+                if page["page_name"] not in section_header_line and len(section_header_line) != 0:
+                    cell_to_insert = f"{header_column["DESCRIPTION"]}{Row_num_to_insert}"
+                    sheet[cell_to_insert]= section_header_line
+                    Row_num_to_insert=Row_num_to_insert+1
+                else:
+                    continue
+            Row_num_to_insert=Row_num_to_insert+1
+            for section_item_counter in range(0,len(page["Contractor"][0]["bill_section_items"][section_header_counter])):
+                combained_section_item = {
+                    "ITEM":"",
+                    "DESCRIPTION": "",
+                    "QUANTITY": 0,
+                    "UNIT": "",
+                    "RATE": [],
+                    "AMOUNT SAR RIYAL": []
+                }
+                # get the items from each contractor combine and store it as dict 
+                for contractor in range(0, number_of_contractors):
+                    print(page["page_name"])
+                    print(page["Contractor"][contractor]["contractor_name"])
+                    print(page["Contractor"][contractor]["bill_section_items"][section_header_counter])
+                    print(page["Contractor"][contractor]["bill_section_items"][section_header_counter][section_item_counter])
+
+                    item = page["Contractor"][contractor]["bill_section_items"][section_header_counter][section_item_counter]
+                    if contractor == 0:
+                        combained_section_item["ITEM"]=item["ITEM"]
+                        combained_section_item["DESCRIPTION"]=item["DESCRIPTION"]
+                        combained_section_item["QUANTITY"]=item["QUANTITY"]
+                        combained_section_item["UNIT"]=item["UNIT"]
+                        combained_section_item["RATE"].append(item["RATE"])
+                        combained_section_item["AMOUNT SAR RIYAL"].append(item["AMOUNT SAR RIYAL"])
+                    else:
+                        combained_section_item["RATE"].append(item["RATE"])
+                        combained_section_item["AMOUNT SAR RIYAL"].append(item["AMOUNT SAR RIYAL"])
+                
+                for key, value in combained_section_item.items():
+                    if key in commonColumns:
+                        cell_to_insert = f"{header_column[key]}{Row_num_to_insert}"
+                        sheet[cell_to_insert]=value
+                        if key == "UNIT":
+                            sheet[cell_to_insert].alignment = Alignment(horizontal="center", vertical="center")
+                    else:
+                        for i in range(0,len(value)):
+                            cell_to_insert = f"{header_column[key][i]}{Row_num_to_insert}"
+                            sheet[cell_to_insert]=value[i]
+                            if key == "RATE" and value[i]==max(value):
+                                sheet[cell_to_insert].fill = PatternFill(start_color="ff0000", end_color="ff0000", fill_type="solid")
+                Row_num_to_insert=Row_num_to_insert+2
+                
+        for contractor in range(0, number_of_contractors):
+            if contractor == 0:
+                startCell = f"{startColumn}{Row_num_to_insert}"
+                endCell = f"{chr(ord(startColumn) + len(commonColumns)-1)}{Row_num_to_insert}"
+                sheet.merge_cells(f"{startCell}:{endCell}")
+                sheet[startCell]="Total - Saudi Riyal Carried to Collection"
+                sheet[startCell].alignment = Alignment(horizontal="right", vertical="center")
+                sheet[startCell].font = Font(bold=True)
+            cell_to_insert= f"{header_column["AMOUNT SAR RIYAL"][contractor]}{Row_num_to_insert}"
+            total= page["Contractor"][contractor]["total"]
+            sheet[cell_to_insert]= total
+            sheet[cell_to_insert].font = Font(bold=True)
+        Row_num_to_insert=Row_num_to_insert+1
+    del wb["Sheet"]
+    wb.save("combined.xlsx")
+
+    
+# getFormattedSheet(formattedSheet)
